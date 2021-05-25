a593ede83c7b3a278c4ed312f2c6ded00b8cb9c2
PRADIP-1596 <9187pradipyadav@gmail.com>
2021-05-25 14:24:22 +0530

    win by 2 digit difference

---
+++ flipcoin.sh
@@ -0,0 +1,34 @@
+  
+#!/bin/bash -x
+heads=1
+tails=0
+headCount=0
+tailCount=0
+for (( i=1; i>0; i++ ))
+do
+
+	coin=$((RANDOM%2));
+	if [ $coin -eq 1 ];
+	then
+		headCount=$(( $headCount + 1 ))
+		result=$((headCount - tilaCount))
+		if [ $result -eq 10 ]
+		then
+			echo "heads win by 2 digit diff";
+			echo $i
+			exit
+		fi
+	fi
+
+	if [ $coin -eq 0 ];
+        then
+               	tailCount=$(( $tailCount + 1 ))
+		result2=$((tailCount - headCount))
+                if [ $result2 -eq 10 ]
+		then
+                	echo "tails win by 2digit diff";
+			echo $i
+                	exit
+		fi
+        fi
+done
